83af1af7967519558f0ca45c625aacd40b998295
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-06-25 20:45:36 +0200

    Create mysql_backup.sh

---
+++ MysqlProj/mysql_backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script uses rsync to take a backup of the folder /var/lib/mysql , 
+# where the databases of mysql are located on the mysqlsrv box.
+# It will be executed through a root cronjob on the back-srv 
+MYSQLIP="192.168.10.120"
+rsync -ra root@${MYSQLIP}:/var/lib/mysql/ /var/lib/mysql/ 
